288aae1185017723025b5f8c2e0bcdcca810b635
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-12-13 17:05:09 -0800

    fixing broken build

--- src/App.js
+++ src/App.js
@@ -11,63 +11,50 @@ function App() {
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
+    const getFilms = async () => {
+      const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/films`, {
+        headers: {
+          apikey: process.env.REACT_APP_SUPABASE_KEY,
+          Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
+        },
+      });
+
+      const data = await resp.json();
+
+      const newData = data.map((item) => [
+        item.title,
+        item.title.toLowerCase().replace(/ /g, '-'),
+        item.academy_award_nominations,
+        item.box_office_total,
+      ]);
+
+      setFilms(newData);
+      return [films];
+    };
+    const getCharacters = async () => {
+      const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/characters`, {
+        headers: {
+          apikey: process.env.REACT_APP_SUPABASE_KEY,
+          Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
+        },
+      });
+      const data = await resp.json();
+
+      const newData = data.map((item) => {
+        if (item.birth === item.death) {
+          return [item.name, (item.dates = 'Unknown')];
+        } else {
+          return [item.name, (item.dates = `${item.birth}-${item.death}`)];
+        }
+      });
+
+      setCharacters(newData);
+      return [characters];
+    };
+
     getFilms();
     getCharacters();
-  }, []);
-
-  const getFilms = async () => {
-    const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/films`, {
-      headers: {
-        apikey: process.env.REACT_APP_SUPABASE_KEY,
-        Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
-      },
-    });
-
-    const data = await resp.json();
-
-    const newData = data.map((item) => [
-      item.title,
-      item.title.toLowerCase().replace(/ /g, '-'),
-      item.academy_award_nominations,
-      item.box_office_total,
-    ]);
-
-    setFilms(newData);
-    return [films];
-  };
-
-  const getCharacters = async () => {
-    const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/characters`, {
-      headers: {
-        apikey: process.env.REACT_APP_SUPABASE_KEY,
-        Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
-      },
-    });
-    // Add your code here!
-    // 1. Get data using fetch from https://the-one-api.dev/v2/character/
-    // 2. Update the response data with the key `dates` which is a combination of
-    //    the `birth` key and the `death key` separated with a dash. If neither date
-    //    is provided, it should hold the string 'Unknown'
-    //    [
-    //       {name: 'Adanel', birth: "", death: "", dates: "Unknown", ...},
-    //       {name: 'Adrahil I', birth: "Before , TA 1944", death: "Late , Third Age", dates: "Before , TA 1944 - Late , Third Age", ...},
-    //       {name: 'Adrahil II', birth: "TA 2917", death: "TA 3010, dates: "TA 2917 - TA 3010", ...},
-    //    ]
-    // 3. Set the resulting transformation as state using setCharacters
-    // 4. You'll know it works if the characters show up on the page
-    const data = await resp.json();
-
-    const newData = data.map((item) => {
-      if (item.birth === item.death) {
-        return [item.name, (item.dates = 'Unknown')];
-      } else {
-        return [item.name, (item.dates = `${item.birth}-${item.death}`)];
-      }
-    });
-
-    setCharacters(newData);
-    return [characters];
-  };
+  }, [characters, films]);
 
   return (
     <div className="App">
